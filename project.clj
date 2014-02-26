eaa17d9b84aa2ff6ffc66f31617592b868f336ca
Geoffroy Couprie <geo.couprie@gmail.com>
2014-02-26 20:08:06 +0100

    require core.logic

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Protoproof"
   :url "http://example.com/FIXME"
   :license {}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.5"]])
--- src/protoproof/core.clj
+++ src/protoproof/core.clj
@@ -1,4 +1,6 @@
-(ns protoproof.core)
+(ns protoproof.core
+  (:refer-clojure :exclude [==])
+  (:use [clojure.core.logic]))
 
 (defn foo
   "I don't do a whole lot."
